74725905ec5f37d4730d14b4bcadfe47de028347
lmaccherone <larry@maccherone.com>
2020-07-12 19:49:30 -0400

    Can now use Functions for hosting static files - needs some refactoring

--- api/status/index.js
+++ api/status/index.js
@@ -14,7 +14,7 @@ module.exports = async function (context, req) {
   else {
     context.res = {
       status: 400,
-      body: "Please pass a name on the query string or in the request body"
+      body: "Please pass a name on the query string for GET or in the request body as a POST"
     };
   }
 };
