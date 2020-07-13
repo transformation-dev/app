259e41ed736e8c4b4f2a79acd1915208d9baedae
lmaccherone <larry@maccherone.com>
2020-07-12 21:35:21 -0400

    Cleanup use of functions

--- api/status/index.js
+++ api/status/index.js
@@ -1,6 +1,4 @@
 module.exports = async function (context, req) {
-  context.log('JavaScript HTTP trigger function processed a request.');
-
   const name = req.query.name || (req.body && req.body.name)
   if (name) {
     context.res = {
